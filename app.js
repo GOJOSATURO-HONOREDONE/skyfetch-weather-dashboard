68ad1e83e69c05cb98ef64dda1419c96155f01c2
ADITYAJ1234 <42756@yenepoya.edu.in>
2026-02-25 09:53:00 +0530

    Completed Part 1

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 function getWeather() {
     const city = document.getElementById("cityInput").value;
+
+    if (city === "") {
+        document.getElementById("weatherResult").innerHTML =
+            "Please enter a city name.";
+        return;
+    }
+
     document.getElementById("weatherResult").innerHTML =
-        "Weather for " + city + " will appear here.";
+        "Weather for <strong>" + city + "</strong> will appear here.";
 }
